4b8a5bac88f191f25e185dae9ce1f4472b7a912c
Piotr Jaskowiec <piotr.jaskowiec@tietoevry.com>
2024-03-20 16:07:56 +0100

    [TASK-5] Migrate page elements to being static

--- kickstart/init-container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source .venv/bin/activate
-build_command=$(python fetch_db_creds.py --image-name student-crud --tag latest --build True)
-run_command=$(python fetch_db_creds.py --image-name student-crud --tag latest)
-pushd ../
-   #$build_command
-   echo $run_command
-popd
